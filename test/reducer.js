5af928e01763306a3ba80c52a1268c2c9027fa00
Stoeffel <schtoeffel@gmail.com>
2016-05-07 16:39:31 +0200

    add reducer tests

---
+++ test/reducer.js
@@ -0,0 +1,45 @@
+import assert from 'assert'
+import testReducer from 'redux-test-reducer'
+import { ELM, reducer } from '../src'
+
+describe('Reducer', () => {
+  it('should be a function', () => {
+    assert.equal(typeof reducer, 'function')
+  })
+
+  it('returns the payload of an elm-action as the new state', () => {
+    const assertReducer = testReducer(reducer)
+    const from = {
+      init: true
+    }
+    const to = {
+      elm: true
+    }
+    const action = {
+      type: ELM,
+      payload: {
+        elm: true
+      }
+    }
+    assertReducer({
+      from, to, action
+    })
+  })
+
+  it('should return the initial state if not an elm action', () => {
+    const assertReducer = testReducer(reducer)
+    const from = {
+      init: true
+    }
+    const to = from
+    const action = {
+      type: 'OTHER_ACTION',
+      payload: {
+        foo: true
+      }
+    }
+    assertReducer({
+      from, to, action
+    })
+  })
+})
