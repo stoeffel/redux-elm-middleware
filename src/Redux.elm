33eef9e93721b29b6c99c53a95bb72f8b5ff9159
Igor Kuzmenko <igor@kuzzmi.com>
2019-08-30 11:39:08 +0300

    Adds programWithFlagsAndBlacklist

--- src/Redux.elm
+++ src/Redux.elm
@@ -1,4 +1,4 @@
-port module Redux exposing (program, programWithFlags)
+port module Redux exposing (program, programWithFlags, programWithFlagsAndBlacklist)
 
 {-| Redux elm middleware
 @docs program
@@ -88,6 +88,74 @@ programWithFlags app =
 
         wrap update msg model =
             reducer msg <| update msg model
+
+        init flags =
+            let
+                ( initModel, initCmd ) =
+                    app.init flags
+
+                initReduxCmd =
+                    elmToRedux ( initActionType, app.encode initModel )
+            in
+            ( initModel
+            , Cmd.batch
+                [ initCmd
+                , initReduxCmd
+                ]
+            )
+    in
+    Platform.programWithFlags
+        { init = init
+        , update = wrap app.update
+        , subscriptions = app.subscriptions
+        }
+
+
+programWithFlagsAndBlacklist :
+    { init : flags -> ( model, Cmd msg )
+    , update : msg -> model -> ( model, Cmd msg )
+    , encode : model -> Json.Value
+    , subscriptions : model -> Sub msg
+    }
+    -> List (model -> a)
+    -> Program flags model msg
+programWithFlagsAndBlacklist app blacklist =
+    let
+        reducer action ( message, cmd ) =
+            ( message
+            , Cmd.batch
+                [ cmd
+                , ( toString action
+                  , app.encode message
+                  )
+                    |> elmToRedux
+                ]
+            )
+
+        wrap update msg model =
+            let
+                ( new, _ ) =
+                    update msg model
+
+                old =
+                    model
+
+                different =
+                    let
+                        lo =
+                            List.map (\x -> x old) blacklist
+
+                        ln =
+                            List.map (\x -> x new) blacklist
+                    in
+                    List.foldl (==) True <|
+                        List.map (\( x, y ) -> x == y) <|
+                            List.map2 (\x y -> ( x, y )) lo ln
+            in
+            if different then
+                reducer msg <| update msg model
+            else
+                update msg model
 
         init flags =
             let
